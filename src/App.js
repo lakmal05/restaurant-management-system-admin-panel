751e83b809c845bb1389853d1202203cf2088688
Lakmal05 <lakmal@webmotech.com>
2024-09-03 10:20:52 +0530

    udpate app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React from "react";
 
 //import Scss
-import './assets/scss/themes.scss';
+import "./assets/scss/themes.scss";
 
 //imoprt Route
-import Route from './Routes';
+import Route from "./Routes";
 
-
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
+import { ConfigProvider } from "antd";
 
 function App() {
   return (
     <React.Fragment>
-      <Route />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#000",
+            colorPrimaryHover: "#000",
+            borderRadius: 5,
+            activeBg: "#124585",
+            optionSelectedBg: "#e6f4ff",
+            controlItemBgHover: "#e6f4ff",
+          },
+        }}
+      >
+        <Route />
+      </ConfigProvider>
     </React.Fragment>
   );
 }
